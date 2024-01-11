84555eb76b45434e51c214b6d6866eaac58b0ff1
BuildTools <unconfigured@null.spigotmc.org>
2024-01-11 18:54:18 +0000

    A

---
+++ src/main/java/com/thatguycy/worlddynamicsengine/WorldDynamicsEngine.java
@@ -0,0 +1,17 @@
+package com.thatguycy.worlddynamicsengine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WorldDynamicsEngine extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
